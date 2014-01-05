464ac9d073ca11f659f2a7c303b3a67e437693d9
Tamara Temple <tamouse@gmail.com>
2014-01-05 13:14:51 -0600

    Make specs pass.

--- spec/lib/i_know_right_spec.rb
+++ spec/lib/i_know_right_spec.rb
@@ -4,8 +4,4 @@ describe IKnowRight do
   it 'should have a version number' do
     IKnowRight::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
 end
